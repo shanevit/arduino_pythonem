8cb6d238cef7717a522a217de14c169fc8628e34
shanevit <38755375+shanevit@users.noreply.github.com>
2021-10-05 17:47:49 +0200

    Create dohromady.py

---
+++ dohromady.py
@@ -0,0 +1,33 @@
+a = float(input("Zadej prvni cislo: "))
+b = float(input("Zadej druhe cislo: "))
+operace = input("Zadej operaci: ")
+vysledek = 0
+
+
+def porovnavani(a,b):
+    if a > b:
+        print("Prvni cislo je vetsi!")
+    elif b > a:
+        print("Druhe cislo je vetsi!")
+    else:
+        print("Cisla se rovnaji!")
+
+def pocitej(a,b,operace):
+    if operace == "+":
+        vysledek = a + b
+    elif operace == "-":
+        vysledek = a - b
+    elif operace == "*":
+        vysledek = a * b
+    elif operace == "/":
+        vysledek = a / b
+    else:
+        print("Zadavej pouze + - * /")
+
+    print("Vysledek = " + str(vysledek))
+
+
+print("Kalkulacka na pocitani a porovnavani")
+pocitej(a,b,operace)
+porovnavani(a,b)
+print("Hotovo")
